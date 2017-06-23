bc88595b7fe9e1ee5fd3f6cfafd7330bfa42a644
lji5317 <lji5317@gmail.com>
2017-06-23 19:22:52 +0900

    init

---
+++ src/main/java/com/example/starcraft/StartController.java
@@ -0,0 +1,16 @@
+package com.example.starcraft;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by lji5317 on 2017-06-23.
+ */
+@Controller
+public class StartController {
+        @RequestMapping("/creatUnitT")
+    public String creatUnitT() {
+
+        return "";
+    }
+}
